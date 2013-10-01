2d966a86e6deb600aef59e3f29b8b29449feeda6
Kerry Gallagher <k.gallagher.05@gmail.com>
2013-10-01 14:27:11 +0100

    Initial structure and API

---
+++ voxxen.js
@@ -0,0 +1,48 @@
+/* Use: 
+
+    Voxxen.alert({
+        onDismiss: fn
+        onSubmit: fn
+        animate: boolean
+        outerClass: string or array
+    })
+
+*/
+
+(function(root) {
+
+
+    var Voxxen = (function() {
+
+        function createAlert() {
+
+        }
+
+        function createConfirm() {
+
+        }
+
+        function createPrompt() {
+
+        }
+
+        return {
+            alert: function(options) {
+
+            },
+
+            confirm: function(options) {
+
+            },
+
+            prompt: function(options) {
+
+            }
+        }
+    })(); 
+
+    root.Voxxen = Voxxen;
+
+
+
+})(window);
