0240591eb5b0f95695315205963c1d4f4488a705
Kerry Gallagher <k.gallagher.05@gmail.com>
2013-10-01 14:37:17 +0100

    Start filling in the API

--- voxxen.js
+++ voxxen.js
@@ -1,6 +1,7 @@
 /* Use: 
 
     Voxxen.alert({
+        onShow: fn
         onDismiss: fn
         onSubmit: fn
         animate: boolean
@@ -11,32 +12,60 @@
 
 (function(root) {
 
+    var baseDialogue = {
+        show: function() {
+
+        },
+
+        clear: function() {
+
+        },
+
+        build: function() {
+
+        }
+    };
+
+    var alertDialogue = {
+
+    };
+
+    var confirmDialogue = {
+
+    };
+
+    var promptDialogue = {
+
+    };
 
     var Voxxen = (function() {
 
         function createAlert() {
-
+            var alert = Object.create(baseDialogue, alertDialogue);
+            return alert.init();
         }
 
         function createConfirm() {
-
+            var confirm = Object.create(baseDialogue, confirmDialogue);
+            return confirm.init();
         }
 
         function createPrompt() {
-
+            var prompt = Object.create(baseDialogue, promptDialogue);
+            return prompt.init();
         }
 
         return {
             alert: function(options) {
-
+                return createAlert();
             },
 
             confirm: function(options) {
-
+                return createConfirm();
             },
 
             prompt: function(options) {
-
+                return createPrompt();
             }
         }
     })(); 
